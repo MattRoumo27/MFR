870f88eafd653955bca4a897e213f7ef0bc9f593
Matt Roumeliotis <mattromo27@gmail.com>
2021-09-27 19:25:36 -0400

    Made a level select menu that loads in the proper level

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 using UnityEngine.UI;
 using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject loadingScreen;
+    public GameObject levelsPlaceholder;
+    public GameObject levelCardPrefab;
+    public GameObject SelectLevelMenu;
     public Slider slider;
     public TextMeshProUGUI progressText;
 
     public void LoadLevel(int sceneIndex)
     {
+        SelectLevelMenu.SetActive(false);
         StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    public void CreateLevelButtons()
+    {
+        for (int levelNumber = 1; levelNumber < SceneManager.sceneCountInBuildSettings; levelNumber++)
+        {
+            GameObject levelCard = Instantiate(levelCardPrefab, levelsPlaceholder.transform);
+            RectTransform cardRect = (RectTransform)levelCard.transform;
+
+            const float padding = 50;
+            float spacingWidth = cardRect.rect.width + padding;
+
+            levelCard.transform.parent = levelsPlaceholder.transform;
+            levelCard.transform.position = levelCard.transform.position + new Vector3((levelNumber - 1) * spacingWidth, 0, 0);
+
+            Button cardButton = levelCard.GetComponent<Button>();
+            TextMeshProUGUI cardText = levelCard.GetComponentInChildren<TextMeshProUGUI>();
+            cardText.text = $"Level {levelNumber}";
+
+            int buttonSceneIndex = levelNumber;
+            cardButton.onClick.AddListener(() => LoadLevel(buttonSceneIndex));
+        }
     }
 
     public void QuitGame()
