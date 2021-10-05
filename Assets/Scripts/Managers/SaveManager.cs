89ba260f09805dde05a0c23970494f795c53b059
Matt Roumeliotis <mattromo27@gmail.com>
2021-10-05 19:58:31 -0400

    Reorganized methods

--- Assets/Scripts/Managers/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -6,41 +6,6 @@ public static class SaveManager
 {
     #region Level Information
 
-    #region SaveLevelCollection
-    public static void SaveLevelCollection(LevelCollectionData levelCollection)
-    {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/levelData.bin";
-        using (FileStream stream = new FileStream(path, FileMode.Create))
-        {
-            formatter.Serialize(stream, levelCollection);
-            Debug.Log("Successfully Saved Entire Level Data to " + path);
-        }
-    }
-    #endregion
-
-    #region LoadLevelCollection
-    public static LevelCollectionData LoadLevelCollection()
-    {
-        string path = Application.persistentDataPath + "/levelData.bin";
-        if (File.Exists(path))
-        {
-            BinaryFormatter formatter = new BinaryFormatter();
-            using (FileStream stream = new FileStream(path, FileMode.Open))
-            {
-                LevelCollectionData levelCollection = formatter.Deserialize(stream) as LevelCollectionData;
-
-                return levelCollection;
-            }
-        }
-        else
-        {
-            Debug.LogWarning("Level Collection Save File Could Not Be Found in " + path);
-            return null;
-        }
-    }
-    #endregion
-
     #region SaveLevel
     public static void SaveLevel(LevelData levelData)
     {
@@ -78,6 +43,41 @@ public static class SaveManager
         }
 
         return null;
+    }
+    #endregion
+
+    #region SaveLevelCollection
+    public static void SaveLevelCollection(LevelCollectionData levelCollection)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/levelData.bin";
+        using (FileStream stream = new FileStream(path, FileMode.Create))
+        {
+            formatter.Serialize(stream, levelCollection);
+            Debug.Log("Successfully Saved Entire Level Data to " + path);
+        }
+    }
+    #endregion
+
+    #region LoadLevelCollection
+    public static LevelCollectionData LoadLevelCollection()
+    {
+        string path = Application.persistentDataPath + "/levelData.bin";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                LevelCollectionData levelCollection = formatter.Deserialize(stream) as LevelCollectionData;
+
+                return levelCollection;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Level Collection Save File Could Not Be Found in " + path);
+            return null;
+        }
     }
     #endregion
 
